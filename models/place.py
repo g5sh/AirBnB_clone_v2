ad2d2ee33712df3a1eaf3b717d7f9991ff63beb5
g5sh <eagomez2387@gmail.com>
2020-04-01 17:06:20 -0500

    [UPDATE]MODEL PLACE AND AMENITIES: Add new funtions to user and place and amenity models

--- models/place.py
+++ models/place.py
@@ -8,10 +8,10 @@ import models
 
 
 place_amenity = Table('place_amenity', Base.metadata,
-                      Column('place_id', String(60)
-                             ForeignKey('place.id'), nullable=False)
-                      Column('amenity_id', String(60)
-                             ForeignKey('amenity.id'), nullable=False))
+                      Column('place_id', String(60),
+                             ForeignKey('places.id'), nullable=False),
+                      Column('amenity_id', String(60),
+                             ForeignKey('amenities.id'), nullable=False))
 
 class Place(BaseModel, Base):
     """This is the class for Place

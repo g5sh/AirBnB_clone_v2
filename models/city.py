bca3911e9b333e671f2e6a187d4e47b91b31120c
Virteip <sergiopietri@gmail.com>
2020-04-01 11:15:48 -0500

    [Add] place relationship

--- models/city.py
+++ models/city.py
@@ -14,3 +14,5 @@ class City(BaseModel, Base):
     __tablename__ = 'cities'
     state_id = Column(String(128), nullable=False)
     name = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship("Place", cascade='all, delete-orphan',
+                          backref='cities')

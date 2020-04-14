f378e8efdd78d11cc71911b4cf2cec268e41672b
g5sh <eagomez2387@gmail.com>
2020-04-14 16:14:59 -0500

    [FIX]FIX: Fix point 2 and 3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -49,16 +49,16 @@ def do_deploy(archive_path):
     name = archive_path.split('/')[1]
     try:
         put(archive_path, '/tmp/')
-        run("mkdir -p /data/web_static/releases/{}".format(name))
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}"
+        run("sudo mkdir -p /data/web_static/releases/{}".format(name))
+        run("sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}"
             .format(name, name))
-        run("rm /tmp/{}".format(name))
-        run("mv /data/web_static/releases/{}/web_static/*\
+        run("sudo rm /tmp/{}".format(name))
+        run("sudo mv /data/web_static/releases/{}/web_static/*\
         /data/web_static/releases/{}".format(name, name))
-        run("rm -rf /data/web_static/releases/{}/web_static"
+        run("sudo rm -rf /data/web_static/releases/{}/web_static"
             .format(name))
-        run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s /data/web_static/releases/{}/ /data/web_static/current"
             .format(name))
         print("New version deployed")
         return(True)

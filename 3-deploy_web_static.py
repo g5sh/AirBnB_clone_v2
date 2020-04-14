282a917e127acfb19215bd504a96d289e0d4bdde
g5sh <eagomez2387@gmail.com>
2020-04-14 16:29:36 -0500

    [FIX]FIX: Fix points 1, 2 and 3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -15,46 +15,65 @@ env.user = 'ubuntu'
 env.hosts = ['35.185.87.99', '54.196.186.129']
 
 def do_pack():
-    '''Packes web_static in tgz format'''
-    n = datetime.now()
-    name = "web_static_{}{}{}{}{}{}.tgz".format(
-        n.year, n.month, n.day, n.hour, n.minute, n.second)
-    local('sudo mkdir -p versions')
-    local("sudo tar -cvzf versions/{} web_static".format(name))
-    size = os.stat("versions/{}".format(name)).st_size
-    print("web_static packed: versions/{} -> {}".format(name, size))
-    path = "versions/{}".format(name)
-    return(path)
+    """
+    Create folder backup with date and tgz extension
+    """
+    print("Packing web_static to versions/web_static_%s.tgz" %
+          (datetime.now().strftime('%Y%m%d%H%M%S')))
+
+    with hide('running'):
+        versions_dir = local('mkdir -p versions')
+
+    tar = local('tar -cvzf versions/web_static_%s.tgz web_static/' %
+                (datetime.now().strftime('%Y%m%d%H%M%S')))
+
+    with hide('running'):
+        file_size = local('wc -c < versions/web_static_{}.tgz'.
+                          format(datetime.now().strftime('%Y%m%d%H%M%S')),
+                          capture=True)
+
+    print("web_static packed: versions/web_static_{:s}.tgz -> {:}Bytes".
+          format(datetime.now().strftime('%Y%m%d%H%M%S'), file_size))
+
+    with hide('running'):
+        archive_path = "versions/web_static_{:s}.tgz".\
+                             format(datetime.now().strftime('%Y%m%d%H%M%S'))
+    return archive_path
 
 
 def do_deploy(archive_path):
+    """
+    Upload backup to server
+    """
+    archive_path = do_pack()
 
-    if not archive_path:
-        return(False)
-    name = archive_path.split('/')[1]
-    try:
+    if archive_path:
+        file_name = archive_path.split('/')[1]
+        clean_name = file_name.split('.')[0]
         put(archive_path, '/tmp/')
-        run("sudo mkdir -p /data/web_static/releases/{}".format(name))
-        run("sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}"
-            .format(name, name))
-        run("sudo rm /tmp/{}".format(name))
-        run("sudo mv /data/web_static/releases/{}/web_static/*\
-        /data/web_static/releases/{}".format(name, name))
-        run("sudo rm -rf /data/web_static/releases/{}/web_static"
-            .format(name))
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s /data/web_static/releases/{}/ /data/web_static/current"
-            .format(name))
-        print("New version deployed")
-        return(True)
-    except BaseException:
-        return(False)
+        run('sudo mkdir -p /data/web_static/releases/{}'.format(clean_name))
+        run('sudo tar -xzf /tmp/{0} -C /data/web_static/releases/{1}'.
+            format(file_name, clean_name))
+        run('sudo mv /data/web_static/releases/{0}/web_static/* '
+            '/data/web_static/releases/{1}'.format(clean_name, clean_name))
+        run('sudo rm /tmp/{}'.format(file_name))
+        run('sudo rm -rf /data/web_static/current')
+        run('sudo rm -rf /data/web_static/releases/{}/web_static/'.
+            format(clean_name))
+        run('sudo ln -sf /data/web_static/releases/{} /data/web_static/current'.
+            format(clean_name))
+        print('New version deployed!')
+        return True
+    else:
+        return False
 
 
 def deploy():
-
-    try:
-        path = do_pack()
-    except BaseException:
-        return(False)
-    do_deploy(path)
+    """
+    Full deployment
+    """
+    archive_path = do_pack()
+    if path.exists(archive_path):
+        return do_deploy(archive_path)
+    else:
+        return False

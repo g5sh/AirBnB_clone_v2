b6f325eff4395d2f49b3775d5b0d85d9151063c2
g5sh <eagomez2387@gmail.com>
2020-04-22 12:53:03 -0500

    [ADD]POINT 3: ADD POIMT 3 FIX 5

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,31 +1,35 @@
 #!/usr/bin/python3
-"""script that starts a Flask web application"""
+'''crates a flask web application'''
 from flask import Flask
 
-
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def hello():
-    return('Hello HBNB!')
+    """Hello"""
+    return 'Hello HBNB!'
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route('/hbnb')
 def hbnb():
-    return('HBNB')
+    """hbnb"""
+    return 'HBNB'
 
 
-@app.route('/c/<text>')
-def c_isfun(text):
-    return('C %s' % text.replace("_", " "))
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """c is fun"""
+    return 'C %s' % text
 
 
-@app.route('/python', defaults={'text': 'is cool'})
-@app.route('/python/(<text>)')
-def Python_is_cool(text):
-    return('Python %s' % text.replace("_", " "))
+@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_is_cool(text):
+    """python is cool"""
+    return 'Python %s' % text.replace("_", " ")
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port='5000')

72ce8ccaf126cc54fb4e3358f572bf11c8f18d97
g5sh <eagomez2387@gmail.com>
2020-04-22 15:26:21 -0500

    [ADD]POINT 5: Aad point 5 and htnml template [FIX]: 8

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -22,25 +22,25 @@ def hbnb():
 @app.route('/c/<text>', strict_slashes=False)
 def c_is_fun(text):
     """c is fun"""
-    return('C {}'.format(text.replace("_", " ")))
+    return('C {:d}'.format(text.replace("_", " ")))
 
 
 @app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_is_cool(text):
     """python is cool"""
-    return('Python {}'.format(text.replace("_", " ")))
+    return('Python {:d}'.format(text.replace("_", " ")))
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def number(n):
+def is_n_number(n):
     """number"""
-    return('{} is a number'.format(n))
+    return "{:d} is a number".format(n)
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def html_n(n):
-    """ html5 """
+def template(n):
+    """ HTML 5 """
     return render_template('5-number.html', value=n)
 
 

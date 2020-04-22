e1817fe5e183f72851570e15a41e01c07b282d01
g5sh <eagomez2387@gmail.com>
2020-04-22 12:59:27 -0500

    [ADD]POINT 4: ADD POIMT 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+'''crates a flask web application'''
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    """Hello"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """hbnb"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """c is fun"""
+    return 'C %s' % text
+
+
+@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_is_cool(text):
+    """python is cool"""
+    return 'Python %s' % text.replace("_", " ")
+
+
+@app.route('/number/<n>', strict_slashes=False)
+def number():
+    return '%d is a number' % n
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')

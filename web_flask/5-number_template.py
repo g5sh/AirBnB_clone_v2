e878a05d28a73664da28f1ed27e59fce97ce22fa
g5sh <eagomez2387@gmail.com>
2020-04-22 19:09:44 -0500

    [ADD]POINT 5: Aad point 5 and htnml template [FIX]: 13

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,58 +1,53 @@
 #!/usr/bin/python3
-"""crates a flask web application"""
-
-
+"""task 5
+"""
 from flask import Flask
 from flask import render_template
-
-
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello():
-    """Hello
+    """ display greeting
     """
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """hbnb
+def hello_hbnb():
+    """ display hbnb
     """
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def c_is_fun(text):
-    """c is fun
+def what_is_c(text):
+    """ display what is c
     """
-    return "C " + text.replace('_', ' ')
+    return "C {}".format(text.replace('_', ' '))
 
 
-@app.route('/python', strict_slashes=False)
+@app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python_is_cool(text='is_cool'):
-    """python is cool
+def what_is_python(text="is cool"):
+    """ display what is python
     """
-    return "Python " + text.replace('_', ' ')
+    return "Python {}".format(text.replace('_', ' '))
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def is_n_number(n):
-    """number
+def to_n_or_not_to_n(n):
+    """ display if n is integer
     """
-    return "{:d} is a number".format(n)
+    return "{} is a number".format(n)
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def template(n):
-    """HTML 5
+def render_n_if_n(n):
+    """ display template if n is integer
     """
-    return render_template('5-number.html', value=n)
+    return render_template('5-number.html', n=n)
 
 
-if __name__ == "__main__":
-    """main
-    """
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
